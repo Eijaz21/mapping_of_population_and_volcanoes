bb37145517670657df1c39103907451f2b21826f
Eijaz Baganee <eijaz.baganee@mu.ey.com>
2021-11-13 21:46:52 +0400

    Added html to marker

--- map1.py
+++ map1.py
@@ -1,8 +1,24 @@
 import folium
-map = folium.Map(location=[-20.16291007051114, 57.52132921046838], tiles = "Stamen Terrain") 
-
-feature_group = folium.FeatureGroup(name="My Map")
-feature_group.add_child(folium.Marker(location=[-20.26291007051114,57.72132921046838], popup="Hi I am a marker", icon=folium.Icon(color='green')))
-map.add_child(feature_group)
-
+import pandas
+ 
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+ 
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+ 
+map = folium.Map(location=[-20.16291007051114, 57.52132921046838], tiles = "Stamen Terrain")
+fg = folium.FeatureGroup(name = "My Map")
+ 
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
+ 
+map.add_child(fg)
 map.save("Map1.html")
